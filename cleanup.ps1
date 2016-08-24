dbf5665d66f12089ed7c9e8e0e86516336ef60dd
Garret Rumohr <garretr@gmail.com>
2016-08-23 23:38:35 -0500

    cleanup.ps1 cleans the public desktop of all files, detection.ps1 checks if directory should be cleaned

---
+++ cleanup.ps1
@@ -0,0 +1,33 @@
+﻿Param(
+    [string]$exclude = ""
+    )
+
+#setup excluded files list.
+#these will be spared from deletion.
+#Entered parameter as comma "," separated list (no spaces)
+$excludeList = @()
+
+ForEach ($value in $exclude.Split(","))
+{
+    $excludeList += $value
+}
+
+$excludeList
+#Get parts in target directory for comparision.
+#Delete if not an excluded value
+$dirfiles = Get-ChildItem "${env:PUBLIC}\Desktop"
+
+foreach ($file in $dirfiles)
+{
+    if($excludeList -contains $file)
+    {
+    }
+    else
+    {
+        If ($file | Test-Path){
+            $file | Remove-Item
+            }
+    }
+}
+
+
---
+++ detection.ps1
@@ -0,0 +1,36 @@
+﻿Param(
+    [string]$exclude = ""
+    )
+
+#setup excluded files list.
+#these will be spared from deletion.
+#Entered parameter as comma "," separated list (no spaces)
+$excludeList = @()
+
+ForEach ($value in $exclude.Split(","))
+{
+    $excludeList += $value
+}
+
+$desktopState = 0
+
+#Get parts in target directory for comparision.
+#Delete if not an excluded value
+$dirfiles = Get-ChildItem "${env:PUBLIC}\Desktop"
+
+
+foreach ($file in $dirfiles)
+{
+    if($excludeList -contains $file)
+    {
+    }
+    else
+    {
+       $desktopState += 1
+    }
+}
+
+If ($desktopState -eq 0)
+{
+Write-Host "No extra files found"
+}
